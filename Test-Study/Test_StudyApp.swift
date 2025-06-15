10900eccf66209a0b918930ca765e0d201ff2c31
OneTen19 <khy000426@naver.com>
2025-06-16 00:09:03 +0900

    Initial Commit

---
+++ Test-Study/Test_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_StudyApp.swift
+//  Test-Study
+//
+//  Created by OneTen on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Test_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
